d1e38cf7e8f714d38626f905cd3a6cf618691b02
Ridhijain987 <85636663+Ridhijain987@users.noreply.github.com>
2022-05-15 20:11:39 +0530

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -21,3 +21,35 @@ class HelloWorld {
         }
     }
 }
+
+
+
+
+
+// LinkedList program2 -----------> Take a list as input from user and delete numbers greater than 25
+
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        LinkedList <Integer> list= new LinkedList<Integer>();
+        System.out.println("Enter the size of Linked List");
+        int num=sc.nextInt();
+        System.out.println("Enter the values in the list");
+        while(num>0){
+            list.addLast(sc.nextInt());
+            num--;
+        }
+        for(int i=0;i<list.size();i++){
+            if(list.get(i)>25){
+                list.remove(i);
+            }
+        }
+        System.out.print(list);
+    }
+}
+
+
+
+
+
